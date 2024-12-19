948c6e444e836d94959af7b98f43e7f149368a7b
Neil Sierra <sierra.neilalfredngis@gmail.com>
2024-12-19 10:44:06 +0800

    a

--- ITC Final Project/scripts/change-theme.js
+++ ITC Final Project/scripts/change-theme.js
@@ -3,6 +3,18 @@ const switchTheme = (function () {
   const themes = [
     function darkTheme() {
       const root = document.documentElement;
+      const background = document.getElementById('background3');
+      background.style.setProperty('background-color', '#000000');
+      root.style.setProperty('--navigation','#121213');
+      root.style.setProperty('--sidebar', '#161618');
+      root.style.setProperty('--content1', '#1B1B1F');
+      root.style.setProperty('--content2', '#202127');
+      root.style.setProperty('--text1', '#E0E0D8');
+      root.style.setProperty('--text2', '#9B9DA5');
+      root.style.setProperty('--border', '#2e2e32');
+      root.style.setProperty('--scroll1', '#686868');
+      root.style.setProperty('--scroll2', '#424242');
+      root.style.setProperty('--theme', '#000000');
       root.style.setProperty('--bg-nav-color', '#161618');
       const moon = document.getElementById('moon');
       moon.style.setProperty('opacity', '1');
@@ -11,6 +23,18 @@ const switchTheme = (function () {
     },
     function lightTheme() {
       const root = document.documentElement;
+      const background = document.getElementById('background3');
+      background.style.setProperty('background-color', '#FFFFFF');
+      root.style.setProperty('--navigation', '#ededec');
+      root.style.setProperty('--sidebar', '#e9e9e7');
+      root.style.setProperty('--content1', '#e4e4e0');
+      root.style.setProperty('--content2', '#dfded8');
+      root.style.setProperty('--text1', '#1f1f27');
+      root.style.setProperty('--text2', '#64625a');
+      root.style.setProperty('--border', '#d1d1cd');
+      root.style.setProperty('--scroll1', '#979797');
+      root.style.setProperty('--scroll2', '#bdbdbd');
+      root.style.setProperty('--theme', '#FFFFFFF');
       const moon = document.getElementById('moon');
       moon.style.setProperty('opacity', '0');
       const sun = document.getElementById('sun');
